c75e6445df82b21213272df1935babe5beb6137d
Faisal Alshathri <Faisal-sh@msn.com>
2022-11-23 10:54:45 +0000

    add initial project files

---
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/Team11RecruitmentAndAdmissionsApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11RecruitmentAndAdmissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team11RecruitmentAndAdmissionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/Team11RecruitmentAndAdmissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team11RecruitmentAndAdmissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
