80086c0e09cbf25548e59443fdb90bcbafd0cebb
Richard <richardekong290@gmail.com>
2022-12-02 21:08:57 +0100

    rename this file

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/FileUploadService.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/FileUploadService.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
-public interface FileService {
+public interface FileUploadService {
 
     Path uploadFile(MultipartFile file);
 
