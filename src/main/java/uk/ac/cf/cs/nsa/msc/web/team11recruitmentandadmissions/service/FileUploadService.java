a97aafad066add79d328f801eb1466c95307669f
Richard <richardekong290@gmail.com>
2022-12-02 21:38:15 +0100

    modify this interface

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/FileUploadService.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/FileUploadService.java
@@ -2,19 +2,15 @@ package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
 public interface FileUploadService {
 
-    Path uploadFile(MultipartFile file);
-
     Path uploadFiles(LinkedHashMap<String, MultipartFile> files);
 
-    FileInputStream uploadFileInputStream(MultipartFile file);
-
-    LinkedList<FileInputStream> uploadFileInputStreams(LinkedHashMap<String, MultipartFile> files);
+    LinkedList<InputStream> uploadFileInputStreams(LinkedHashMap<String, MultipartFile> files);
 
 }
