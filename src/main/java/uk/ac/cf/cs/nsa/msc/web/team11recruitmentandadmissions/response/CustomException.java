ec61ed308b0396eb52ecd0a148d81ae63e68605b
Richard <richardekong290@gmail.com>
2022-12-02 19:26:46 +0100

    CHANGES FOR SUCCESS AND ERROR HANDLING
    - Create CustomException as a java class that stores information or data
      about http status code and error messages of errors that may arise
      during user interaction with this system.
    
    - Create Response for storing data about http status code,  descriptions about success and error
    
    - Create global exception handlers to catch exceptions and display
      information about exceptions from any module of the server

---
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/response/CustomException.java
@@ -0,0 +1,24 @@
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.response;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+
+@Data
+public class CustomException extends RuntimeException{
+
+    private final HttpStatus status;
+
+
+    public CustomException(String message, @NotNull HttpStatus status){
+        super(message);
+        this.status = status;
+    }
+
+    public CustomException(@NotNull HttpStatus status){
+        super(status.getReasonPhrase());
+        this.status = status;
+    }
+}
+
