b36ebf813044f3d0aeb222d1b4095a1d0546125e
Richard <richardekong290@gmail.com>
2022-12-02 20:46:17 +0100

    create file upload service

---
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/FileService.java
@@ -0,0 +1,20 @@
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
+public interface FileService {
+
+    Path uploadFile(MultipartFile file);
+
+    Path uploadFiles(LinkedHashMap<String, MultipartFile> files);
+
+    FileInputStream uploadFileInputStream(MultipartFile file);
+
+    LinkedList<FileInputStream> uploadFileInputStreams(LinkedHashMap<String, MultipartFile> files);
+
+}
