762a74cc43d727628c608775b35df55fd37cdda4
Nishchay Bhatt <bhattn1@cardiff.ac.uk>
2022-12-06 16:13:02 +0000

    Make changes to application status code based on Janet Israel's latest message

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/model/ApplicationStatusCode.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/model/ApplicationStatusCode.java
@@ -1,7 +1,20 @@
 package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model;
 
 public enum ApplicationStatusCode {
-    APPLICATION("A");
+    APPLICATION("A"),
+    GATHERED_FIELD_ON_HOLD("GF"),
+    OTHER_REASON_ON_HOLD("OR"),
+    AWAITING_INTERVIEW_ON_HOLD("OH"),
+    REJECTED("R"),
+    CONDITIONAL_OFFER_MADE("C"),
+    CONDITIONAL_OFFER_FIRMED("CF"),
+    CONDITIONAL_OFFER_INSURANCE("CI"),
+    CONDITIONAL_OFFER_DECLINED("CD"),
+    UNCONDITIONAL_OFFER_MADE("U"),
+    UNCONDITIONAL_OFFER_FIRMED("UF"),
+    UNCONDITIONAL_OFFER_INSURANCE("UI"),
+    UNCONDITIONAL_OFFER_DECLINED("UD"),
+    WITHDRAWN("W");
 
     private String code;
 
