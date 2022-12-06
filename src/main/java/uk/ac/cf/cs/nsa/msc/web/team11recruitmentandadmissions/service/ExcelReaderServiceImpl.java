45cbafe3ad71008732d9e46f1d7f7484a770fd34
Faisal Alshathri <Faisal-sh@msn.com>
2022-12-06 17:05:34 +0000

    create method called saveCandidatesToDatabase which implement readCandidatesFromExcelSheet method to read excel shhet first and store all row in that sheet as LinkedList of candidates then passing that LinkedList to be saved in databas by using savell method in repository

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/ExcelReaderServiceImpl.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/ExcelReaderServiceImpl.java
@@ -7,15 +7,20 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.*;
+import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.repository.CandidateRepository;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class ExcelReaderServiceImpl implements ExcelReaderService {
+
+    private CandidateRepository candidateRepository;
 
 
     private DataFormatter cellFormatter;
@@ -54,4 +59,13 @@ public class ExcelReaderServiceImpl implements ExcelReaderService {
         return candidates;
     }
 
+
+    public void saveCandidatesToDatabase(InputStream inputStream){
+            try {
+                List<Candidate> candidates = readCandidatesFromExcelSheet(inputStream);
+                this.candidateRepository.saveAll(candidates);
+            } catch (Exception e) {
+                e.printStackTrace();
+        }
+    }
 }
