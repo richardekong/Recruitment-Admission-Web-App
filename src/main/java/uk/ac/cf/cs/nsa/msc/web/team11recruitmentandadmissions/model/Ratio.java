7324a2f87a2c08df09f511bbef6e13dbea2f37c7
Nishchay Bhatt <bhattn1@cardiff.ac.uk>
2022-12-13 19:02:33 +0000

    Implement Historical Data Service

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/model/Ratio.java
+++
@@ -1,2 +0,0 @@
-package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model;public class Ratio {
-}
---
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/repository/HistoricalDataRepository.java
@@ -0,0 +1,8 @@
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.HistoricalData;
+
+public interface HistoricalDataRepository extends JpaRepository<HistoricalData,String> {
+
+}
