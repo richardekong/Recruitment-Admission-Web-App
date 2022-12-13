17d2b74a65c9de5a570111670680a8034785f7a3
Faisal Alshathri <Faisal-sh@msn.com>
2022-12-13 21:32:49 +0000

    create HistoricalDataExcelSheetService interface with two function , first one to create the header of the created excel sheet , second function to pass the created excel sheet to be downloaded

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/HistoricalDataExcelSheetService.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/HistoricalDataExcelSheetService.java
@@ -3,7 +3,7 @@ package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.service;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface CreateHistoricalDataExcelSheet {
+public interface HistoricalDataExcelSheetService {
 
     void writeHeaderRow();
     void createExcelSheet(HttpServletResponse response) throws IOException;
