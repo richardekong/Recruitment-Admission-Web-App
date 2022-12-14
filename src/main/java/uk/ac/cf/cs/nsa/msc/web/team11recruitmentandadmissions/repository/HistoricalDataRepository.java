8fa3e4d365fcede1afc49e698a228c04ed4bf68f
Nishchay Bhatt <BhattN1@cardiff.ac.uk>
2022-12-14 18:15:54 +0000

    declare a helper method to select all ratio values from the HistoricalData table within the database

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/repository/HistoricalDataRepository.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/repository/HistoricalDataRepository.java
@@ -1,8 +1,13 @@
 package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.HistoricalData;
+
+import java.util.List;
 
 public interface HistoricalDataRepository extends JpaRepository<HistoricalData,Integer> {
 
+    @Query("SELECT ratio from HistoricalData")
+    List<Double> selectRatiosFromHistorical ();
 }
