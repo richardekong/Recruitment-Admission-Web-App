84c92d19cfeb19fecc797feb88f3f15808331b24
Nishchay Bhatt <BhattN1@cardiff.ac.uk>
2022-12-14 19:19:51 +0000

    Create a REST controller to request ratio prediction services

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/controller/OfferPredictionsController.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/controller/OfferPredictionsController.java
@@ -1,2 +1,24 @@
-package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.controller;public class OfferPredictionsController {
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.service.PredictionService;
+
+@RestController
+public class OfferPredictionsController {
+
+    private PredictionService predictionService;
+
+    @Autowired
+    public void setPredictionService(PredictionService service){
+        this.predictionService = service;
+    }
+
+    @GetMapping("/ratio")
+    public double predictRatio(){
+        return predictionService.eliminateOutliers();
+    }
 }
+
