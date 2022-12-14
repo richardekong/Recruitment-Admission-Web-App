f4ac5a077c502455fdd243612806836b8f9eb0a4
Nishchay Bhatt <BhattN1@cardiff.ac.uk>
2022-12-14 19:55:30 +0000

    Update class for Places Offered by adding unique randomly generated ID

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/model/PlacesOffered.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/model/PlacesOffered.java
@@ -1,2 +1,23 @@
-package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model;public class PlacesOffered {
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class PlacesOffered {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer placesOffered;
 }
