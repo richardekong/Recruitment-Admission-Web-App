c0f512119d123fe1a913ecf310d43aff7b482a35
Richard Willie Ekong <ekongr@cardiff.ac.uk>
2023-02-15 01:14:32 +0000

    Make the following changes
    - Refactor CandidatePagingAndSortingRepository to fix sorting bugs
    - Fix sorting bugs in CandidateController, CandidateSortingService and candidates.html
    - Create SortUtils and UpdateUtils to handle sorting and update operation in a neat manner

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/repository/CandidatePagingAndSortingRepository.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/repository/CandidatePagingAndSortingRepository.java
@@ -11,28 +11,5 @@ import java.time.LocalDate;
 
 public interface CandidatePagingAndSortingRepository extends PagingAndSortingRepository<Candidate, String> {
 
-
-    Slice<Candidate> findCandidateByStudentNo(String studentNo, Pageable pageable);
-
-    Slice<Candidate> findCandidateBySurname(String surname, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByApplicationStatusCode(ApplicationStatusCode code, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByOfferConditions(String offerCondition, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByRecordFirstCreated(LocalDate createdOn, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByPersonalID(String personalID, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByDateOfBirth(LocalDate dateOfBirth, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByTotalPersonalStatementScore(int score, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByTotalInterviewScore(int score, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByInviteInterview(YesOrNoOption option, Pageable pageable);
-
-    Slice<Candidate> findCandidatesByOfferEmailSent(YesOrNoOption option, Pageable pageable);
-
 }
 
--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/CandidateSortingServiceImpl.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/CandidateSortingServiceImpl.java
@@ -1,79 +1,128 @@
 package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.ApplicationStatusCode;
 import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.Candidate;
-import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.YesOrNoOption;
 import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.repository.CandidatePagingAndSortingRepository;
+import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.repository.CandidateRepository;
+import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.response.CustomException;
 
-import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
 
 @Service
-public class CandidateSortingServiceImpl implements CandidateSortingService{
+public class CandidateSortingServiceImpl implements CandidateSortingService {
 
-    private CandidatePagingAndSortingRepository repository;
+    private CandidatePagingAndSortingRepository pagingAndSortingRepo;
+
+    private CandidateRepository candidateRepository;
 
     @Autowired
-    void setRepository(CandidatePagingAndSortingRepository repository){
-        this.repository = repository;
+    void setPagingAndSortingRepo(
+            CandidatePagingAndSortingRepository pagingAndSortingRepo) {
+        this.pagingAndSortingRepo = pagingAndSortingRepo;
+    }
+
+    @Autowired
+    void setCandidateRepository(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
     }
 
     @Override
-    public Slice<Candidate> findCandidateByStudentNo(String studentNo, Pageable pageable) {
-        return repository.findCandidateByStudentNo(studentNo, pageable);
+    public Slice<Candidate> sortNothing() {
+        return createSlice(candidateRepository.findAll(), PageRequest.of(0, 100));
     }
 
     @Override
-    public Slice<Candidate> findCandidateBySurname(String surname, Pageable pageable) {
-        return repository.findCandidateBySurname(surname, pageable);
+    public Slice<Candidate> sort(Pageable pageable) {
+        return pagingAndSortingRepo.findAll(pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByApplicationStatusCode(ApplicationStatusCode code, Pageable pageable) {
-        return repository.findCandidatesByApplicationStatusCode(code, pageable);
+    public Slice<Candidate> sortByApplicationStatusCode(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getApplicationStatusCode().getCode()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByOfferConditions(String offerCondition, Pageable pageable) {
-        return repository.findCandidatesByOfferConditions(offerCondition, pageable);
+    public Slice<Candidate> sortByGender(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getGender().getGender()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByRecordFirstCreated(LocalDate createdOn, Pageable pageable) {
-        return repository.findCandidatesByRecordFirstCreated(createdOn, pageable);
+    public Slice<Candidate> sortByFeeStatus(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getFeeStatus().getFeeStatus()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByPersonalID(String personalID, Pageable pageable) {
-        return repository.findCandidatesByPersonalID(personalID, pageable);
+    public Slice<Candidate> sortByCorrespondenceLangWelsh(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getCorrespondenceLangWelsh().getOption()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByDateOfBirth(LocalDate dateOfBirth, Pageable pageable) {
-        return repository.findCandidatesByDateOfBirth(dateOfBirth, pageable);
+    public Slice<Candidate> sortByWelshSpeaker(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getWelshSpeaker().getResponse()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByTotalPersonalStatementScore(int score, Pageable pageable) {
-        return repository.findCandidatesByTotalPersonalStatementScore(score, pageable);
+    public Slice<Candidate> sortByContextualFlag(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getContextualFlag().getOption()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByTotalInterviewScore(int score, Pageable pageable) {
-        return repository.findCandidatesByTotalInterviewScore(score, pageable);
+    public Slice<Candidate> sortByKeepingWarmEmailSent(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getKeepingWarmEmailSent().getOption()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByInviteInterview(YesOrNoOption option, Pageable pageable) {
-        return repository.findCandidatesByInviteInterview(option, pageable);
+    public Slice<Candidate> sortByInviteInterview(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getInviteInterview().getOption()), pageable);
     }
 
     @Override
-    public Slice<Candidate> findCandidatesByOfferEmailSent(YesOrNoOption option, Pageable pageable) {
-        return repository.findCandidatesByOfferEmailSent(option, pageable);
+    public Slice<Candidate> sortByFTPChecked(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getFTPChecked().getOption()), pageable);
+    }
+
+    @Override
+    public Slice<Candidate> sortByNonStandardQualificationsChaserEmail(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getNonStandardQualificationsChaserEmail().getOption()), pageable);
+    }
+
+    @Override
+    public Slice<Candidate> sortByOfferEmailSent(Pageable pageable) {
+        return sortBy(comparing(candidate -> candidate.getOfferEmailSent().getOption()), pageable);
+    }
+
+    private Slice<Candidate> createSlice(List<Candidate> content, Pageable pageable) {
+        return new SliceImpl<>(content, pageable, true);
+    }
+
+    private Stream<Candidate> getParallelStreamOfCandidates() {
+        return candidateRepository.findAll().parallelStream();
+    }
+
+    private Slice<Candidate> sortBy(Comparator<Candidate> sortFunction, Pageable pageable) {
+        return pageable.getSort().stream().sorted()
+                .map(order -> order.getDirection().isAscending())
+                .map(isAscending -> isAscending
+                        ? getParallelStreamOfCandidates()
+                        .sorted(sortFunction)
+                        .collect(Collectors.toList())
+                        : getParallelStreamOfCandidates()
+                        .sorted(sortFunction.reversed())
+                        .collect(Collectors.toList()))
+                .map(candidates -> createSlice(candidates, pageable))
+                .findFirst()
+                .orElseThrow(() -> new CustomException("Server error", HttpStatus.INTERNAL_SERVER_ERROR));
     }
 }
 
