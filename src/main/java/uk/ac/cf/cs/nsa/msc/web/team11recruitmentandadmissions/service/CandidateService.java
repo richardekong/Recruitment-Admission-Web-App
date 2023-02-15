82cc5ee89922d03f18447a2602711a0aa2841659
Richard Willie Ekong <ekongr@cardiff.ac.uk>
2023-02-15 01:32:34 +0000

    Begin to implement search functionalities at service level

--- src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/CandidateService.java
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/service/CandidateService.java
@@ -1,8 +1,12 @@
 package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.service;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.ApplicationStatusCode;
 import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.Candidate;
 import uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.model.YesOrNoOption;
 
+import java.time.LocalDate;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,4 +25,26 @@ public interface CandidateService {
     Candidate updateCandidate(Candidate candidateToUpdate);
 
     Integer countCandidatesByOfferEmailSent(YesOrNoOption option);
+
+    Slice<Candidate> findCandidateByStudentNo(String studentNo, Pageable pageable);
+
+    Slice<Candidate> findCandidateBySurname(String surname, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByApplicationStatusCode(ApplicationStatusCode code, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByOfferConditions(String offerCondition, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByRecordFirstCreated(LocalDate createdOn, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByPersonalID(String personalID, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByDateOfBirth(LocalDate dateOfBirth, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByTotalPersonalStatementScore(int score, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByTotalInterviewScore(int score, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByInviteInterview(YesOrNoOption option, Pageable pageable);
+
+    Slice<Candidate> findCandidatesByOfferEmailSent(YesOrNoOption option, Pageable pageable);
 }
---
+++ src/main/java/uk/ac/cf/cs/nsa/msc/web/team11recruitmentandadmissions/utils/SearchUtils.java
@@ -0,0 +1,4 @@
+package uk.ac.cf.cs.nsa.msc.web.team11recruitmentandadmissions.utils;
+
+public class SearchUtils {
+}
