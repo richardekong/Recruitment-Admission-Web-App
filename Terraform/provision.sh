bdec7ee084f294f6d093b7348701d9f1256fe3a9
Richard Willie Ekong <ekongr@cardiff.ac.uk>
2023-05-09 03:34:05 +0100

    Add provisioning bash script

--- Terraform/provision.sh
+++ Terraform/provision.sh
@@ -23,7 +23,7 @@ export OS_USERNAME="c22106964"
 echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
 #read -sr OS_PASSWORD_INPUT
 #export OS_PASSWORD=$OS_PASSWORD_INPUT
-#export OS_PASSWORD=$unipass
+export OS_PASSWORD=$unipass
 # If your configuration has multiple regions, we set that information here.
 # OS_REGION_NAME is optional and only valid in certain environments.
 export OS_REGION_NAME="RegionOne"
